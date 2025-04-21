f9da19f6e45f35deaf2774af9eb8c077a450bc1d
Jaygupta930 <jayguptajaygupta8@gmail.com>
2025-04-21 15:00:24 +0530

    Refactor Navbar component to use NavLink for routing and update styles in navbar.css

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 import './navbar.css'
 
 const Navbar = () => {
   return (
     <nav className='navbar'>
-        <div className='navbar__logo'>
-            <img src='/logo.png' alt='Logo' />
+        <div className='navbar__logo' onClick={() => window.location.href = '/home'}>
+            <img src='/assets/logo.svg' alt='AI Healthcare Logo' />
         </div>
         <div className='navbar__links'>
-            <Link to="/home">Home</Link>
-            <Link to="/upload">Upload</Link>
-            <Link to="/about">About</Link>
-            <Link to="/chatbot">Chatbot</Link>
+            <NavLink to="/home" className={({ isActive }) => isActive ? 'active' : ''}>Home</NavLink>
+            <NavLink to="/upload" className={({ isActive }) => isActive ? 'active' : ''}>Upload</NavLink>
+            <NavLink to="/about" className={({ isActive }) => isActive ? 'active' : ''}>About</NavLink>
+            <NavLink to="/chatbot" className={({ isActive }) => isActive ? 'active' : ''}>Chatbot</NavLink>
         </div>
     </nav>
   )
